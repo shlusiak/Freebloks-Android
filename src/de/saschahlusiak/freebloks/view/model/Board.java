de7999008930cadc4a19058c8a7e15c327c9b33e
Sascha Hlusiak <mail@saschahlusiak.de>
2013-01-31 15:14:33 +0100

    refactoring: fix weird coordinate transformations in previous code
    the top left corner (yellow starting point) has the board coordinates 0/0
    the blue start corner has 0/19
    
    in unified coordinates, the bottom left corner has 0/0

--- src/de/saschahlusiak/freebloks/view/model/Board.java
+++ src/de/saschahlusiak/freebloks/view/model/Board.java
@@ -10,22 +10,25 @@ public class Board extends ViewElement {
 	
 	
 	/**
-	 * Converts a point from model coordinates to (non-uniformed) board coordinates
+	 * Converts a point from model coordinates to (non-uniformed) board coordinates.
+	 * The top-left corner is 0/0, the blue starting point is 0/19
 	 * @param point
 	 * @return point
 	 */
 	public PointF modelToBoard(PointF point) {
-		point.x = point.x + BoardRenderer.stone_size * (float)(model.spiel.m_field_size_x - 1);
-		point.y = BoardRenderer.stone_size * (float)(model.spiel.m_field_size_x - 1) - point.y;
-		
 		point.x = point.x / (BoardRenderer.stone_size * 2.0f);
 		point.y = point.y / (BoardRenderer.stone_size * 2.0f);
 		
+		point.x = point.x + 0.5f * (float)(model.spiel.m_field_size_x - 1);
+		point.y = point.y + 0.5f * (float)(model.spiel.m_field_size_y - 1);
+
 		return point;
 	}
 	
 	/**
 	 * converts p from relative board coordinates, to rotated board coordinates
+	 * relative board coordinates: yellow starting point is 0/0, blue starting point is 0/19
+	 * unified coordinates: bottom left corner is always 0/0
 	 * @param p
 	 */
 	void boardToUnified(PointF p) {
@@ -34,20 +37,20 @@ public class Board extends ViewElement {
 		switch (model.showPlayer) {
 		default:
 		case 0: /* nothing */
+			p.y = model.spiel.m_field_size_y - p.y;
 			break;
 		case 1:
 			tmp = p.x;
-			p.x = model.spiel.m_field_size_x - p.y;
+			p.x = p.y;
 			p.y = tmp;
 			break;
 		case 2: /* 180 degree */
 			p.x = model.spiel.m_field_size_x - p.x;
-			p.y = model.spiel.m_field_size_y - p.y;
 			break;
 		case 3:
 			tmp = p.y;
-			p.y = model.spiel.m_field_size_y - p.x;
-			p.x = tmp;
+			p.y = model.spiel.m_field_size_x - p.x;
+			p.x = model.spiel.m_field_size_y - tmp;
 			break;
 		}
 	}
