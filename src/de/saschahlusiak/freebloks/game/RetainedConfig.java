2bdb1b49318429343dc1411b088c0130cb6105a7
Sascha Hlusiak <mail@saschahlusiak.de>
2013-02-12 13:48:58 +0100

    little refactoring, move RetainedConfig to separate class

---
+++ src/de/saschahlusiak/freebloks/game/RetainedConfig.java
@@ -0,0 +1,8 @@
+package de.saschahlusiak.freebloks.game;
+
+import de.saschahlusiak.freebloks.network.NET_SERVER_STATUS;
+
+class RetainedConfig {
+	SpielClientThread clientThread;
+	NET_SERVER_STATUS lastStatus;
+}
