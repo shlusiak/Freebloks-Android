3b141f8b5a937315c4c3dd0fdd98e84add108125
Sascha Hlusiak <mail@saschahlusiak.de>
2013-03-06 01:24:49 +1100

    undo effect: fix wrong position on fields != 20

--- src/de/saschahlusiak/freebloks/view/effects/StoneUndoEffect.java
+++ src/de/saschahlusiak/freebloks/view/effects/StoneUndoEffect.java
@@ -2,15 +2,18 @@ package de.saschahlusiak.freebloks.view.effects;
 
 import javax.microedition.khronos.opengles.GL10;
 
-import de.saschahlusiak.freebloks.model.Spiel;
 import de.saschahlusiak.freebloks.model.Stone;
 import de.saschahlusiak.freebloks.view.BoardRenderer;
+import de.saschahlusiak.freebloks.view.model.ViewModel;
 
 public class StoneUndoEffect extends AbsStoneEffect {
 	static private final float TIME = 1.1f;
 	
-	public StoneUndoEffect(Stone stone, int player, int x, int y) {
+	ViewModel model;
+	
+	public StoneUndoEffect(ViewModel model, Stone stone, int player, int x, int y) {
 		super(stone, player, x, y);
+		this.model = model;
 	}
 	
 	@Override
@@ -34,9 +37,9 @@ public class StoneUndoEffect extends AbsStoneEffect {
 		gl.glTranslatef(0, z, 0);
 		
 	    gl.glTranslatef(
-	    		-BoardRenderer.stone_size * (float)(Spiel.DEFAULT_FIELD_SIZE_X - 1) + BoardRenderer.stone_size * 2.0f * (float)x,
+	    		-BoardRenderer.stone_size * (float)(model.spiel.m_field_size_x - 1) + BoardRenderer.stone_size * 2.0f * (float)x,
 	    		0,
-	    		-BoardRenderer.stone_size * (float)(Spiel.DEFAULT_FIELD_SIZE_Y- 1) + BoardRenderer.stone_size * 2.0f * (float)y);
+	    		-BoardRenderer.stone_size * (float)(model.spiel.m_field_size_y - 1) + BoardRenderer.stone_size * 2.0f * (float)y);
 		
 	    gl.glRotatef(rot, 0, 1, 0);
 		renderer.renderPlayerStone(gl, player, stone, alpha * BoardRenderer.DEFAULT_ALPHA);
