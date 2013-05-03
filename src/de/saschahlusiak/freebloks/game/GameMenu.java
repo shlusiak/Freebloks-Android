2a889242b26e5fae41da3b982eb7b932b5f0ad1e
Sascha Hlusiak <mail@saschahlusiak.de>
2013-05-03 20:27:05 +0800

    menu: add sound on/off quick button to GameMenu

--- src/de/saschahlusiak/freebloks/game/GameMenu.java
+++ src/de/saschahlusiak/freebloks/game/GameMenu.java
@@ -5,26 +5,24 @@ import de.saschahlusiak.freebloks.R;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
-import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ToggleButton;
 
 public class GameMenu extends Dialog {
+	ToggleButton soundButton;
+	FreebloksActivity activity;
 
 	public GameMenu(Context context) {
 		super(context);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);		
 		setContentView(R.layout.game_menu_dialog);
 		
-		try {
-			PackageInfo pinfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-			((TextView)findViewById(R.id.version)).setText("" + pinfo.versionName);
-		} catch (NameNotFoundException e) {
-			((TextView)findViewById(R.id.version)).setVisibility(View.GONE);
-			e.printStackTrace();
-		}
 		findViewById(R.id.imageView1).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -32,6 +30,31 @@ public class GameMenu extends Dialog {
 				getContext().startActivity(intent);
 			}
 		});
+		soundButton = (ToggleButton)findViewById(R.id.sound_toggle_button);
+		soundButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				activity = (FreebloksActivity)getOwnerActivity();
+				soundButton.setCompoundDrawablesWithIntrinsicBounds(
+						isChecked ? android.R.drawable.ic_lock_silent_mode_off : android.R.drawable.ic_lock_silent_mode,
+						0,
+						0,
+						0);
+				activity.view.model.soundPool.setEnabled(isChecked);
+				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+				Editor editor = prefs.edit();
+				editor.putBoolean("sounds", isChecked);
+				editor.commit();
+			}
+		});
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		activity = (FreebloksActivity)getOwnerActivity();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+		soundButton.setChecked(prefs.getBoolean("sounds", true));
 	}
 
 	@Override
