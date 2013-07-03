f2d2cbe9be5750f244901e203213a43b2b3be445
Sascha Hlusiak <mail@saschahlusiak.de>
2013-07-03 13:00:05 +0800

    lobby: write 'Red: ' instead of 'Client 1:' if color is known

--- src/de/saschahlusiak/freebloks/lobby/ChatEntry.java
+++ src/de/saschahlusiak/freebloks/lobby/ChatEntry.java
@@ -1,8 +1,11 @@
 package de.saschahlusiak.freebloks.lobby;
 
+import android.graphics.Color;
+
 public class ChatEntry {
 	int client;
 	String text, name;
+	int player;
 	
 	public ChatEntry(int client, String text, String name) {
 		this.client = client;
@@ -11,6 +14,23 @@ public class ChatEntry {
 			this.name = name;
 		else
 			name = "Client " + client;
+		
+		player = -1;
+	}
+	
+	void setPlayer(int player) {
+		this.player = player;
+	}
+	
+	int getColor() {
+		final int player_colors[] = { Color.BLUE, Color.YELLOW, Color.RED , Color.GREEN };
+		final int extra_colors[] = { Color.CYAN, Color.WHITE, Color.MAGENTA, Color.LTGRAY };
+
+
+		if (player < 0)
+			return extra_colors[client % extra_colors.length];
+		else
+			return player_colors[player];
 	}
 	
 	@Override
