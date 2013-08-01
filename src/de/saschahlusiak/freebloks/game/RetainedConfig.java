7a37d989bde2d8a1e42f5e52b1fa1ca117593fe9
Sascha Hlusiak <mail@saschahlusiak.de>
2013-08-01 13:36:29 +0800

    game/view: persist intro in retained config to survive rotate

--- src/de/saschahlusiak/freebloks/game/RetainedConfig.java
+++ src/de/saschahlusiak/freebloks/game/RetainedConfig.java
@@ -1,10 +1,12 @@
 package de.saschahlusiak.freebloks.game;
 
 import de.saschahlusiak.freebloks.network.NET_SERVER_STATUS;
+import de.saschahlusiak.freebloks.view.model.Intro;
 import de.saschahlusiak.freebloks.view.model.Sounds;
 
 class RetainedConfig {
 	SpielClientThread clientThread;
 	NET_SERVER_STATUS lastStatus;
 	Sounds soundPool;
+	Intro intro;
 }
