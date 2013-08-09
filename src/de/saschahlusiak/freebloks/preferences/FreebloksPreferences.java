ac3f697fbdd538318b15c693f261e67260955acd
Sascha Hlusiak <mail@saschahlusiak.de>
2013-08-09 16:24:06 +0800

    use getPackageName for package names instead of hardcoded path

--- src/de/saschahlusiak/freebloks/preferences/FreebloksPreferences.java
+++ src/de/saschahlusiak/freebloks/preferences/FreebloksPreferences.java
@@ -27,9 +27,9 @@ public class FreebloksPreferences extends PreferenceActivity implements OnShared
 			public boolean onPreferenceClick(Preference preference) {
 				String url;
 				if (Global.IS_AMAZON)
-					url = Global.getMarketURLString();
+					url = Global.getMarketURLString(getBaseContext());
 				else
-					url = "market://details?id=" + Global.PACKAGE_NAME;
+					url = "market://details?id=" + getApplication().getPackageName();
 				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 				startActivity(intent);
 				return true;
