9b383a0d746e61bda7c1c1f4d261a2320e4d05e0
Sascha Hlusiak <mail@saschahlusiak.de>
2013-08-12 19:13:52 +0800

    donate: fix crash when inventory query failed, handle failure, added retry button

--- src/de/saschahlusiak/freebloks/donate/DonateActivity.java
+++ src/de/saschahlusiak/freebloks/donate/DonateActivity.java
@@ -20,8 +20,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DonateActivity extends Activity implements OnIabSetupFinishedListener, OnIabPurchaseFinishedListener, OnConsumeFinishedListener {
 	IabHelper mHelper;
@@ -38,7 +40,7 @@ public class DonateActivity extends Activity implements OnIabSetupFinishedListen
 		mHelper = new IabHelper(this, Global.base64EncodedPublicKey);
 		mHelper.enableDebugLogging(true);
 		
-		mHelper.startSetup(this);
+		mHelper.startSetup(this);		
 	}
 	
 	@Override
@@ -70,9 +72,30 @@ public class DonateActivity extends Activity implements OnIabSetupFinishedListen
 	}
 
 	@Override
-	public void onIabSetupFinished(IabResult result) {
+	public void onIabSetupFinished(final IabResult result) {
+		findViewById(R.id.next).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				RadioGroup group = (RadioGroup)findViewById(R.id.radioGroup);
+				RadioButton button = (RadioButton)findViewById(group.getCheckedRadioButtonId());
+				if (button == null)
+					return;
+				if (!button.isEnabled())
+					return;
+				
+				SkuDetails detail = (SkuDetails)button.getTag();
+				if (detail == null) {
+					finish();
+					return;
+				}
+				mHelper.launchPurchaseFlow(DonateActivity.this, detail.getSku(), RC_REQUEST, 
+						DonateActivity.this, "abcdef");
+			}
+		});
+
 		if (!result.isSuccess()) {
 			Log.d(tag, "Problem setting up In-app Billing: " + result);
+			Toast.makeText(this, "no internet connection", Toast.LENGTH_LONG).show();
 		} else {
 			List<String> more = new ArrayList<String>();
 			more.add("donation_001");
@@ -84,8 +107,18 @@ public class DonateActivity extends Activity implements OnIabSetupFinishedListen
 				inventory = mHelper.queryInventory(true, more);
 			} catch (IabException e) {
 				e.printStackTrace();
+				Toast.makeText(this, "error querying inventory", Toast.LENGTH_LONG).show();
+				((Button)findViewById(R.id.next)).setText(R.string.donation_retry);
+				findViewById(R.id.next).setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						onIabSetupFinished(result);
+					}
+				});
 				return;
 			}
+			((Button)findViewById(R.id.next)).setText(R.string.donation_next);
 			setupButton(R.id.donation_001, inventory.getSkuDetails("donation_001"));
 			setupButton(R.id.donation_002, inventory.getSkuDetails("donation_002"));
 			setupButton(R.id.donation_003, inventory.getSkuDetails("donation_003"));
@@ -95,25 +128,6 @@ public class DonateActivity extends Activity implements OnIabSetupFinishedListen
 				if (inventory.hasPurchase(sku))
 	                mHelper.consumeAsync(inventory.getPurchase(sku), this);
 			}
-			
-			findViewById(R.id.next).setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					RadioGroup group = (RadioGroup)findViewById(R.id.radioGroup);
-					RadioButton button = (RadioButton)findViewById(group.getCheckedRadioButtonId());
-					if (!button.isEnabled())
-						return;
-					
-					SkuDetails detail = (SkuDetails)button.getTag();
-					if (detail == null) {
-						finish();
-						return;
-					}
-					mHelper.launchPurchaseFlow(DonateActivity.this, detail.getSku(), RC_REQUEST, 
-							DonateActivity.this, "abcdef");
-				}
-			});
 		}
 	}
 
