94e26f831818c85b3a5a686b6a8b0515b2e78379
Sascha Hlusiak <mail@saschahlusiak.de>
2013-10-09 13:07:44 +0800

    lobby: get correct player color for chat and color bar

--- src/de/saschahlusiak/freebloks/lobby/ChatListAdapter.java
+++ src/de/saschahlusiak/freebloks/lobby/ChatListAdapter.java
@@ -3,7 +3,6 @@ package de.saschahlusiak.freebloks.lobby;
 import java.util.List;
 
 import de.saschahlusiak.freebloks.R;
-import de.saschahlusiak.freebloks.network.NET_CHAT;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.Gravity;
@@ -13,9 +12,15 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class ChatListAdapter extends ArrayAdapter<ChatEntry> {
+	int gamemode;
 	
-	public ChatListAdapter(Context context, List<ChatEntry> values) {
+	public ChatListAdapter(Context context, List<ChatEntry> values, int gamemode) {
 		super(context, R.layout.chat_list_item, R.id.textView, values);
+		setGameMode(gamemode);
+	}
+	
+	public void setGameMode(int gamemode) {
+		this.gamemode = gamemode;
 	}
 	
 	@Override
@@ -30,7 +35,7 @@ public class ChatListAdapter extends ArrayAdapter<ChatEntry> {
 			t.setTextColor(Color.LTGRAY);
 			t.setGravity(Gravity.RIGHT);
 		} else {
-			t.setTextColor(c.getColor());
+			t.setTextColor(c.getColor(gamemode));
 			t.setGravity(Gravity.LEFT);
 		}
 		
