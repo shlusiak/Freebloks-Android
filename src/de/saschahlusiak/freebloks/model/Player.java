5fda21ea842e3ffdb408fec7d86fc8ce15c41c47
Sascha Hlusiak <mail@saschahlusiak.de>
2013-11-06 10:08:49 +0800

    game/view: only mark fields as allowed when there is at least one available move

--- src/de/saschahlusiak/freebloks/model/Player.java
+++ src/de/saschahlusiak/freebloks/model/Player.java
@@ -86,15 +86,21 @@ public class Player implements Serializable, Cloneable {
 		for (int x = 0; x < spiel.m_field_size_x; x++){
 			for (int y = 0; y < spiel.m_field_size_y; y++){
 				if (spiel.get_game_field(m_number, y, x) == Stone.FIELD_ALLOWED){
+					int turns_in_pos = 0;
 					for (int n = 0; n < Stone.STONE_COUNT_ALL_SHAPES; n++){
 						Stone stone = m_stone[n];
 						if (stone.get_available() > 0){
-							int pos_turns;
+							int turns;
 
-							pos_turns = stone.calculate_possible_turns_in_position(spiel, m_number, y, x);
-							m_number_of_possible_turns += pos_turns;
-							m_position_points += pos_turns * stone.get_stone_position_points() * stone.get_stone_points();
+							turns = stone.calculate_possible_turns_in_position(spiel, m_number, y, x);
+							turns_in_pos += turns;
+							m_position_points += turns * stone.get_stone_position_points() * stone.get_stone_points();
 						}
+					}
+					m_number_of_possible_turns += turns_in_pos;
+					if (turns_in_pos == 0) {
+						/* there is no available turn in this position. mark as not allowed */
+						spiel.set_field_free_bit(m_number, y, x);
 					}
 				} else if (spiel.get_game_field(y, x) == m_number)
 					m_stone_points++;
