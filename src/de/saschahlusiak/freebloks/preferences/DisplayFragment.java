d12fc0e1b9297cc7835509e5f3f8bdf134b65e1a
Sascha Hlusiak <mail@saschahlusiak.de>
2013-12-22 16:44:39 +0800

    preferences: support headers for devices with multipane support

---
+++ src/de/saschahlusiak/freebloks/preferences/DisplayFragment.java
@@ -0,0 +1,39 @@
+package de.saschahlusiak.freebloks.preferences;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+import de.saschahlusiak.freebloks.R;
+
+public class DisplayFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences_display);
+    }
+    
+    @Override
+    public void onResume() {    	
+		SharedPreferences prefs = getPreferenceScreen().getSharedPreferences();
+		onSharedPreferenceChanged(prefs, "theme");
+		prefs.registerOnSharedPreferenceChangeListener(this);
+		super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    	super.onPause();
+    }
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		if (key.equals("theme")) {
+			ListPreference pref = (ListPreference)findPreference(key);
+			pref.setSummary(pref.getEntry());
+		}
+	}
+}
---
+++ src/de/saschahlusiak/freebloks/preferences/InterfaceFragment.java
@@ -0,0 +1,14 @@
+package de.saschahlusiak.freebloks.preferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import de.saschahlusiak.freebloks.R;
+
+public class InterfaceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences_interface);
+    }    
+}
