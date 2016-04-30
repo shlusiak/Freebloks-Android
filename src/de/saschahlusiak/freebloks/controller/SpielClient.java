4c9c39b80eb971b4bca45217e7f0358734f66c83
Sascha Hlusiak <mail@saschahlusiak.de>
2016-04-30 13:10:30 +0800

    spielclient: fix NPE when disconnecting without socket being connected

--- src/de/saschahlusiak/freebloks/controller/SpielClient.java
+++ src/de/saschahlusiak/freebloks/controller/SpielClient.java
@@ -96,7 +96,8 @@ public class SpielClient {
 				if(client_socket.isConnected())
 					client_socket.shutdownInput();
 				client_socket.close();
-				sendThread.quit();
+				if (sendThread != null)
+					sendThread.quit();
 				sendThread = null;
 			} catch (IOException e) {
 				e.printStackTrace();
