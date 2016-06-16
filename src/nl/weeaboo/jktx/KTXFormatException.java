a88c59f3e1b17cf76c3e94478b87e89a9441dd20
Sascha Hlusiak <mail@saschahlusiak.de>
2016-06-16 21:36:40 +0800

    libs: replace jktx.jar with source files

---
+++ src/nl/weeaboo/jktx/KTXFormatException.java
@@ -0,0 +1,33 @@
+/* JKTX
+ * 
+ * Copyright (c) 2011 Timon Bijlsma
+ *   
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+   
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package nl.weeaboo.jktx;
+
+public class KTXFormatException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public KTXFormatException(String message) {
+		this(message, null);
+	}
+	public KTXFormatException(String message, Throwable t) {
+		super(message, t);
+	}
+	
+}
