579ea85de73d24d92f332922b0f7670afd7fd482
Sascha Hlusiak <mail@saschahlusiak.de>
2017-06-18 09:30:26 +0800

    nits

--- src/main/java/de/saschahlusiak/freebloks/AboutActivity.java
+++ src/main/java/de/saschahlusiak/freebloks/AboutActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
