d503acecce05ab092ac4d481a089176aa7dc16ab
Sascha Hlusiak <mail@saschahlusiak.de>
2017-12-30 13:55:00 +0800

    jni: support overwriting of client name in server

--- jni/spielserver.cpp
+++ jni/spielserver.cpp
@@ -347,14 +347,18 @@ void CSpielServer::process_message(int client,NET_HEADER* data)
 			   	So werden den Spielern wieder die Clients zugeordnet */
 				spieler[n]=clients[client];
 			}
-			/* setze Spielernamen */
-			if (names[client] == NULL) {
-				if (ntohs(data->data_length) > sizeof(NET_HEADER)) {
-					req->name[sizeof(req->name) - 1] = '\0';
-					if (strlen((char*)req->name) > 0) {
-						/* store client name */
-						names[client] = strdup((char*)req->name);
-					}
+
+
+			if (ntohs(data->data_length) > sizeof(NET_HEADER)) {
+				req->name[sizeof(req->name) - 1] = '\0';
+				if (strlen((char*)req->name) > 0) {
+					// TODO: Allow setting name per player, not per client
+
+					/* store client name */
+					if (names[client] != NULL)
+						free(names[client]);
+
+					names[client] = strdup((char*)req->name);
 				}
 			}
 
