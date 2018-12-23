0ddddd8fb22a55d10336b235af04fd1a7eb07622
Sascha Hlusiak <mail@saschahlusiak.de>
2018-12-23 20:53:37 +0800

    Show paired bluetooth devices

--- src/main/java/de/saschahlusiak/freebloks/game/JoinDialog.java
+++ src/main/java/de/saschahlusiak/freebloks/game/JoinDialog.java
@@ -7,10 +7,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -24,10 +24,15 @@ import de.saschahlusiak.freebloks.R;
 public class JoinDialog extends Dialog implements RadioGroup.OnCheckedChangeListener, View.OnClickListener, TextWatcher {
 	private static final String tag = JoinDialog.class.getSimpleName();
 
+	// TODO: implement bluetooth
+	private static final boolean ENABLE_BLUETOOTH = false;
+
 	private EditText name, server;
 	private OnStartCustomGameListener listener;
 	private RadioGroup serverType;
 	private Button hostGame;
+
+	private ViewGroup bluetoothList;
 	private BluetoothAdapter bluetoothAdapter;
 
 	private SharedPreferences prefs;
@@ -49,7 +54,11 @@ public class JoinDialog extends Dialog implements RadioGroup.OnCheckedChangeList
 
 	private void updateOkButtonEnabled() {
 		boolean enabled = true;
-		if (serverType.getCheckedRadioButtonId() == R.id.radioButton2 && getCustomServer().isEmpty())
+		int checkedId = serverType.getCheckedRadioButtonId();
+		if (checkedId == R.id.radioButton2 && getCustomServer().isEmpty())
+			enabled = false;
+
+		if (checkedId == R.id.radioButton3)
 			enabled = false;
 
 		findViewById(android.R.id.button1).setEnabled(enabled);
@@ -95,15 +104,17 @@ public class JoinDialog extends Dialog implements RadioGroup.OnCheckedChangeList
 		server.setText(prefs.getString("custom_server", ""));
 		server.addTextChangedListener(this);
 
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		bluetoothList = findViewById(R.id.bluetoothList);
+		bluetoothList.setVisibility(View.GONE);
+		if (bluetoothAdapter == null || !ENABLE_BLUETOOTH) {
+			// bluetooth not supported
+			findViewById(R.id.radioButton3).setVisibility(View.GONE);
+		}
+
 		serverType = findViewById(R.id.server_type);
 		serverType.setOnCheckedChangeListener(this);
-
 		((RadioButton)findViewById(R.id.radioButton1)).setChecked(true);
-
-		// TODO: implement bluetooth
-		findViewById(R.id.radioButton3).setVisibility(View.GONE);
-
-		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 	}
 
 	@Override
@@ -146,40 +157,59 @@ public class JoinDialog extends Dialog implements RadioGroup.OnCheckedChangeList
 	@Override
 	public void onCheckedChanged(RadioGroup radioGroup, int i) {
 		server.setVisibility(i == R.id.radioButton2 ? View.VISIBLE : View.GONE);
-		hostGame.setVisibility(i == R.id.radioButton2 ? View.VISIBLE : View.INVISIBLE);
+		hostGame.setVisibility((i == R.id.radioButton2 || i == R.id.radioButton3) ? View.VISIBLE : View.INVISIBLE);
+		bluetoothList.setVisibility(i == R.id.radioButton3 ? View.VISIBLE : View.GONE);
 
+		// wifi
 		if (i == R.id.radioButton2) {
 			server.requestFocus();
+		}
+
+		// bluetooth
+		if (i == R.id.radioButton3) {
+			updateDeviceList();
 		}
 		updateOkButtonEnabled();
 	}
 
-	private void updateDeviceList(BluetoothDeviceAdapter adapter) {
-		Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+	private void updateDeviceList() {
+		final View.OnClickListener deviceSelectedListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				BluetoothDevice device = (BluetoothDevice) v.getTag();
 
-		adapter.clear();
+				Log.i(tag, "Device selected: " + device.getName());
+			}
+		};
+
+		TextView t;
+
+		bluetoothList.removeAllViews();
+		Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+		Log.d(tag, "Paired devices: " + pairedDevices.size());
+
+		if (!bluetoothAdapter.isEnabled() || pairedDevices.isEmpty()) {
+			View v = getLayoutInflater().inflate(R.layout.join_bluetooth_device, bluetoothList, false);
+			v.findViewById(R.id.image).setVisibility(View.GONE);
+			t = v.findViewById(android.R.id.text1);
+			// TODO: translate me
+			t.setText("Bluetooth turned off or no devices found");
+
+			bluetoothList.addView(v, new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+			return;
+		}
+
 		for (BluetoothDevice device: pairedDevices) {
 			Log.d(tag, String.format("device %s [%s]", device.getName(), device.getAddress()));
-		}
-		adapter.addAll(pairedDevices);
-		adapter.notifyDataSetChanged();
-	}
 
-	private static class BluetoothDeviceAdapter extends ArrayAdapter<BluetoothDevice> {
+			View v = getLayoutInflater().inflate(R.layout.join_bluetooth_device, bluetoothList, false);
 
-		public BluetoothDeviceAdapter(Context context) {
-			super(context, R.layout.join_bluetooth_device);
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = LayoutInflater.from(getContext()).inflate(R.layout.join_bluetooth_device, parent, false);
-			BluetoothDevice device = getItem(position);
-
-			TextView t = v.findViewById(android.R.id.text1);
+			t = v.findViewById(android.R.id.text1);
 			t.setText(device.getName());
+			v.setTag(device);
+			v.setOnClickListener(deviceSelectedListener);
 
-			return v;
+			bluetoothList.addView(v, new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 		}
 	}
 }
