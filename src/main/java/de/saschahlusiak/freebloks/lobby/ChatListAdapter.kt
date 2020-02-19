6767c34a1bc786cd2dd250a702f81c3a6e415462
Sascha Hlusiak <mail@saschahlusiak.de>
2020-02-20 07:56:23 +0800

    Convert ChatListAdapter to Kotlin

---
+++ src/main/java/de/saschahlusiak/freebloks/lobby/ChatListAdapter.kt
@@ -0,0 +1,29 @@
+package de.saschahlusiak.freebloks.lobby
+
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import de.saschahlusiak.freebloks.R
+import de.saschahlusiak.freebloks.model.GameMode
+import kotlinx.android.synthetic.main.chat_list_item.view.*
+
+class ChatListAdapter(context: Context, values: List<ChatEntry>, var gameMode: GameMode) : ArrayAdapter<ChatEntry>(context, R.layout.chat_list_item, R.id.textView, values) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = super.getView(position, convertView, parent)
+        val c = getItem(position) ?: return view
+
+        view.textView.apply {
+            if (c.isServerMessage()) {
+                gravity = Gravity.RIGHT
+                setTextColor(Color.LTGRAY)
+            } else {
+                gravity = Gravity.LEFT
+                setTextColor(c.getColor(context, gameMode))
+            }
+        }
+        return view
+    }
+}
