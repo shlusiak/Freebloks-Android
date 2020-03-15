df1df35bb82b54a61302af8ab9426c9232e7533d
Sascha Hlusiak <mail@saschahlusiak.de>
2020-03-15 08:02:56 +0800

    Nits

--- src/main/java/de/saschahlusiak/freebloks/AboutActivity.kt
+++ src/main/java/de/saschahlusiak/freebloks/AboutActivity.kt
@@ -1,27 +1,17 @@
 package de.saschahlusiak.freebloks
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.about_activity.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.about_activity)
-        if (resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK != Configuration.SCREENLAYOUT_SIZE_XLARGE) {
-            val params = window.attributes
-            params.width = ViewGroup.LayoutParams.MATCH_PARENT
-            window.attributes = params
-        }
 
-        findViewById<View>(R.id.ok).setOnClickListener { finish() }
-        @SuppressLint("SetTextI18n")
-        findViewById<TextView>(R.id.version).text = "v${BuildConfig.VERSION_NAME}"
-        findViewById<TextView>(R.id.url1).text = Global.getMarketURLString(packageName)
+        setContentView(R.layout.about_activity)
+
+        ok.setOnClickListener { finish() }
+        version.text = BuildConfig.VERSION_NAME
+        url.text = Global.getMarketURLString(packageName)
     }
 }
