0b6af8b40b50b36b46abaa9e92706dd5fc3d3499
Sascha Hlusiak <mail@saschahlusiak.de>
2020-08-01 12:00:21 +0800

    nit

--- src/main/java/de/saschahlusiak/freebloks/app/App.kt
+++ src/main/java/de/saschahlusiak/freebloks/app/App.kt
@@ -1,8 +1,10 @@
 package de.saschahlusiak.freebloks.app
 
 import android.app.Application
+import androidx.annotation.Keep
 import de.saschahlusiak.freebloks.DependencyProvider
 
+@Keep
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
