2ba14afdba86f60682e4c00a9ba75903a509f905
Sascha Hlusiak <mail@saschahlusiak.de>
2020-08-04 10:26:54 +0800

    Massage SceneTest

--- src/test/java/de/saschahlusiak/freebloks/view/scene/SceneTest.kt
+++ src/test/java/de/saschahlusiak/freebloks/view/scene/SceneTest.kt
@@ -11,6 +11,27 @@ class SceneTest {
 
     private lateinit var game: Game
 
+    private inline fun Scene.Down(x: Float, y: Float, block: Scene.() -> Unit = {}) {
+        handlePointerDown(PointF(x, y))
+        block()
+    }
+
+    private inline fun Scene.Up(x: Float, y: Float, block: Scene.() -> Unit = {}) {
+        handlePointerUp(PointF(x, y))
+        block()
+    }
+
+    private inline fun Scene.MoveTo(x: Float, y: Float, block: Scene.() -> Unit = {}) {
+        handlePointerMove(PointF(x, y))
+        block()
+    }
+
+    private inline fun Scene.Tap(x: Float, y: Float, block: Scene.() -> Unit = {}) {
+        handlePointerDown(PointF(x, y))
+        handlePointerUp(PointF(x, y))
+        block()
+    }
+
     @Before
     fun setup() {
         scene = Scene(null, null, null)
@@ -93,8 +114,6 @@ class SceneTest {
 
     @Test
     fun test_commitStone() {
-        val wheel = scene.wheel
-        val stone = scene.currentStone
         val board = game.board
 
         scene.snapAid = true
@@ -102,46 +121,50 @@ class SceneTest {
         game.currentPlayer = 0
         game.setPlayerType(0, Game.PLAYER_LOCAL)
 
-        assertEquals(-1, wheel.currentPlayer)
-        wheel.update(0)
-        assertEquals(21, wheel.stones.size)
-        assertEquals(0, wheel.currentPlayer)
+        with(scene) {
+            assertEquals(-1, wheel.currentPlayer)
+            wheel.update(0)
+            assertEquals(21, wheel.stones.size)
+            assertEquals(0, wheel.currentPlayer)
+            assertEquals(Wheel.Status.IDLE, wheel.status)
 
-        assertEquals(Wheel.Status.IDLE, wheel.status)
-        // Grab the stone from here. The wheel is not moved, so it is all the way to the left
-        scene.handlePointerDown(PointF(0.0f, 18.0f))
-        assertEquals(Wheel.Status.SPINNING, wheel.status)
-        assertEquals(CurrentStone.Status.IDLE, stone.status)
-        assertEquals(1, wheel.currentStone?.shape?.number)
+            // Grab the stone from here. The wheel is not moved, so it is all the way to the left
+            Down(0.0f, 18.0f) {
+                assertEquals(Wheel.Status.SPINNING, wheel.status)
+                assertEquals(CurrentStone.Status.IDLE, currentStone.status)
+                assertEquals(1, wheel.currentStone?.shape?.number)
+            }
 
-        // Move to the center of the board, which will take it out of the wheel into currentStone
-        scene.handlePointerMove(PointF(0.0f, 0.0f))
-        assertEquals(Wheel.Status.IDLE, wheel.status)
-        assertEquals(1, stone.stone?.shape?.number)
-        assertEquals(CurrentStone.Status.DRAGGING, stone.status)
-        assertEquals(9.0f, stone.pos.x, 1.0f)
-        assertEquals(9.0f, stone.pos.y, 1.0f)
-        assertFalse(stone.isValid)
-        assertEquals(1, wheel.currentStone?.shape?.number)
+            // Move to the center of the board, which will take it out of the wheel into currentStone
+            MoveTo(0.0f, 0.0f) {
+                assertEquals(Wheel.Status.IDLE, wheel.status)
+                assertEquals(1, currentStone.stone?.shape?.number)
+                assertEquals(CurrentStone.Status.DRAGGING, currentStone.status)
+                assertEquals(9.0f, currentStone.pos.x, 1.0f)
+                assertEquals(9.0f, currentStone.pos.y, 1.0f)
+                assertFalse(currentStone.isValid)
+                assertEquals(1, wheel.currentStone?.shape?.number)
+            }
 
-        scene.handlePointerMove(PointF(-7.9f, 8.5f))
+            MoveTo(-7.9f, 8.5f)
+            val turn = currentStone.asTurn()!!
 
-        val turn = stone.asTurn()!!
-        assertEquals(Turn(0, 1, 18, -1, Orientation.Default), turn)
-        assertTrue(stone.isValid)
-        assertTrue(board.isValidTurn(turn))
+            Up(-7.9f, 8.5f) {
+                assertEquals(Turn(0, 1, 18, -1, Orientation.Default), turn)
+                assertTrue(currentStone.isValid)
+                assertTrue(board.isValidTurn(turn))
+            }
 
-        // simulate a tap gesture
-        scene.handlePointerUp(PointF(-7.9f, 8.5f))
-        scene.handlePointerDown(PointF(-7.9f, 8.5f))
-        scene.handlePointerUp(PointF(-7.9f, 8.5f))
+            // simulate a tap gesture
+            Tap(-7.9f, 8.5f) {
+                assertEquals(CurrentStone.Status.IDLE, currentStone.status)
+                assertNull(currentStone.stone)
+                assertNull(wheel.currentStone)
+                assertFalse(currentStone.isValid)
 
-        assertEquals(CurrentStone.Status.IDLE, stone.status)
-        assertNull(stone.stone)
-        assertNull(wheel.currentStone)
-        assertFalse(stone.isValid)
-
-        // the stone wasn't actually set but only requested to be
-        assertTrue(board.isValidTurn(turn))
+                // the stone wasn't actually set but only requested to be
+                assertTrue(board.isValidTurn(turn))
+            }
+        }
     }
 }
