1483e956911eec085144460a78792b9e6b373fe4
Sascha Hlusiak <mail@saschahlusiak.de>
2020-08-11 11:05:39 +0800

    Rebuild textures, add xhdpi and hdpi previews

--- themes/src/main/java/de/saschahlusiak/freebloks/theme/AssetThemes.kt
+++ themes/src/main/java/de/saschahlusiak/freebloks/theme/AssetThemes.kt
@@ -23,54 +23,63 @@ private class AssetTheme(
 class AssetThemes : ThemeProvider {
     private val FloweryCloth = AssetTheme(
         R.string.theme_flowery_cloth,
-        R.drawable.texture_table_1,
+        R.drawable.texture_table_1_preview,
         "texture_table_1",
         "texture_table_cloth_1"
     )
 
     private val StripedCloth = AssetTheme(
         R.string.theme_striped_cloth,
-        R.drawable.texture_table_2,
+        R.drawable.texture_table_2_preview,
         "texture_table_2",
         "texture_table_cloth_2"
     )
 
     private val Wood = AssetTheme(
         R.string.theme_wood,
-        R.drawable.texture_wood_fine,
+        R.drawable.texture_wood_fine_preview,
         "texture_wood_fine",
         "texture_wood"
     )
 
     private val Metal = AssetTheme(
         R.string.theme_metal,
-        R.drawable.texture_metal,
+        R.drawable.texture_metal_preview,
         "texture_metal"
     )
 
     private val Bricks = AssetTheme(
         R.string.theme_bricks,
-        R.drawable.texture_bricks,
+        R.drawable.texture_bricks_preview,
         "texture_bricks"
     )
 
     private val Carpet = AssetTheme(
         R.string.theme_carpet,
-        R.drawable.texture_carpet_blue,
+        R.drawable.texture_carpet_blue_preview,
         "texture_carpet_blue"
     )
     private val Velvet = AssetTheme(
         R.string.theme_velvet,
-        R.drawable.texture_velvet,
+        R.drawable.texture_velvet_preview,
         "texture_velvet"
     )
     private val Grass = AssetTheme(
         R.string.theme_grass,
-        R.drawable.texture_grass,
+        R.drawable.texture_grass_preview,
         "texture_grass"
     )
 
     override fun getAllThemes(context: Context): Collection<Theme> {
-        return listOf(FloweryCloth, StripedCloth, Wood, Metal, Bricks, Carpet, Velvet, Grass)
+        return listOf(
+            FloweryCloth,
+            StripedCloth,
+            Wood,
+            Metal,
+            Bricks,
+            Carpet,
+            Velvet,
+            Grass
+        )
     }
 }
