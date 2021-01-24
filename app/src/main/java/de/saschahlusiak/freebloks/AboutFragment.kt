32be2c617292ed6ca8403b13037067e1e46a9f0d
Sascha Hlusiak <mail@saschahlusiak.de>
2021-01-24 22:49:56 +0800

    Add Donate button to about fragment

--- app/src/main/java/de/saschahlusiak/freebloks/AboutFragment.kt
+++ app/src/main/java/de/saschahlusiak/freebloks/AboutFragment.kt
@@ -1,8 +1,10 @@
 package de.saschahlusiak.freebloks
 
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import de.saschahlusiak.freebloks.donate.DonateActivity
 import de.saschahlusiak.freebloks.utils.MaterialDialogFragment
 import kotlinx.android.synthetic.main.about_activity.*
 
@@ -14,6 +16,10 @@ class AboutFragment : MaterialDialogFragment(R.layout.about_activity) {
         ok.setOnClickListener { dismiss() }
         version.text = BuildConfig.VERSION_NAME
         url.text = Global.getMarketURLString(BuildConfig.APPLICATION_ID)
+
+        donate.setOnClickListener {
+            startActivity(Intent(requireContext(), DonateActivity::class.java))
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
