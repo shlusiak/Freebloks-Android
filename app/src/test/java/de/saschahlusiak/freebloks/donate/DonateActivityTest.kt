1acdaa07e50adff8099cbcb479fa7fd0ecd3878d
Sascha Hlusiak <mail@saschahlusiak.de>
2022-02-18 21:42:46 +1000

    Attempt to fix Robolectric tests

--- app/src/test/java/de/saschahlusiak/freebloks/donate/DonateActivityTest.kt
+++ app/src/test/java/de/saschahlusiak/freebloks/donate/DonateActivityTest.kt
@@ -4,30 +4,41 @@ import android.app.Application
 import android.view.View
 import android.widget.Button
 import androidx.core.view.isVisible
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import de.saschahlusiak.freebloks.Global
 import de.saschahlusiak.freebloks.R
-import de.saschahlusiak.freebloks.app.App
+import de.saschahlusiak.freebloks.utils.AnalyticsProvider
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
 import org.robolectric.annotation.Config
+import javax.inject.Inject
 
 @RunWith(RobolectricTestRunner::class)
-@Config(application = App::class)
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
 class DonateActivityTest {
 
     @get:Rule
-    val rule = ActivityScenarioRule(DonateActivity::class.java)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun checkDisplayStateForFlavour() {
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             if (Global.IS_GOOGLE) {
                 assertTrue(it.findViewById<View>(R.id.donateThankYou).isVisible)
                 assertTrue(it.findViewById<View>(R.id.donationsGroup).isVisible)
@@ -43,23 +54,23 @@ class DonateActivityTest {
                 assertTrue(it.findViewById<View>(R.id.donationPaypal).isVisible)
                 assertTrue(it.findViewById<View>(R.id.donateButtonGroup).isVisible)
             }
-        }
+        }.close()
     }
 
     @Test
     fun skipFinishesActivity() {
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             it.findViewById<Button>(R.id.skip).performClick()
 
             assertTrue(it.isFinishing)
-        }
+        }.close()
     }
 
     @Test
     fun nextButtonClicked() {
         val app = ApplicationProvider.getApplicationContext<Application>()
 
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             it.findViewById<Button>(R.id.next).performClick()
 
             if (Global.IS_GOOGLE) {
@@ -75,42 +86,42 @@ class DonateActivityTest {
                 assertTrue(it.findViewById<View>(R.id.donationsGroup).isVisible)
                 assertTrue(it.findViewById<View>(R.id.donateThankYou).isVisible)
             }
-        }
+        }.close()
     }
 
     @Test
     fun onFreebloksClick() {
         val app = ApplicationProvider.getApplicationContext<Application>()
 
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             it.findViewById<View>(R.id.donationFreebloksVip).performClick()
 
             val startedActivityIntent = Shadows.shadowOf(app).nextStartedActivity
             assertTrue(startedActivityIntent.filterEquals(it.freebloksVipIntent))
-        }
+        }.close()
     }
 
     @Test
     fun onPaypalClick() {
         val app = ApplicationProvider.getApplicationContext<Application>()
 
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             it.findViewById<View>(R.id.donationPaypal).performClick()
 
             val startedActivityIntent = Shadows.shadowOf(app).nextStartedActivity
             assertTrue(startedActivityIntent.filterEquals(it.paypalIntent))
-        }
+        }.close()
     }
 
     @Test
     fun onLitecoinClick() {
         val app = ApplicationProvider.getApplicationContext<Application>()
 
-        rule.scenario.onActivity {
+        ActivityScenario.launch(DonateActivity::class.java).onActivity {
             it.findViewById<View>(R.id.donationLitecoin).performClick()
 
             val startedActivityIntent = Shadows.shadowOf(app).nextStartedActivity
             assertTrue(startedActivityIntent.filterEquals(it.litecoinIntent))
-        }
+        }.close()
     }
 }
