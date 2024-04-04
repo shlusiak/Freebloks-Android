fe7d0203b62e7ff5ac0eb7709908dd1d932fc82a
Sascha Hlusiak <mail@saschahlusiak.de>
2024-04-05 00:35:27 +0800

    Convert RulesActivity to Compose

--- app/src/main/kotlin/de/saschahlusiak/freebloks/rules/RulesActivity.kt
+++ app/src/main/kotlin/de/saschahlusiak/freebloks/rules/RulesActivity.kt
@@ -3,40 +3,32 @@ package de.saschahlusiak.freebloks.rules
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import de.saschahlusiak.freebloks.databinding.RulesActivityBinding
+import de.saschahlusiak.freebloks.app.AppTheme
 import de.saschahlusiak.freebloks.utils.AnalyticsProvider
-import de.saschahlusiak.freebloks.utils.viewBinding
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class RulesActivity : AppCompatActivity() {
     private val youtubeLink = "https://www.youtube.com/watch?v=pc8nmWpcQWs"
 
-    private val binding by viewBinding(RulesActivityBinding::inflate)
-
     @Inject
     lateinit var analytics: AnalyticsProvider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        binding.youtube.setOnClickListener { onYoutubeButtonClick() }
+        setContent {
+            AppTheme {
+                RulesScreen(
+                    onBack = { finish() },
+                    onWatchVideo = ::onYoutubeButtonClick
+                )
+            }
+        }
 
         analytics.logEvent("rules_show", null)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun onYoutubeButtonClick() {
