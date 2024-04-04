d22544c5ca6026d5cba6b0c0cd2c0f6d770a8389
Sascha Hlusiak <mail@saschahlusiak.de>
2024-04-04 19:48:07 +0800

    Fixup new game screen

--- app/src/main/kotlin/de/saschahlusiak/freebloks/game/newgame/NewGameScreen.kt
+++ app/src/main/kotlin/de/saschahlusiak/freebloks/game/newgame/NewGameScreen.kt
@@ -1,6 +1,5 @@
 package de.saschahlusiak.freebloks.game.newgame
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -30,7 +29,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import de.saschahlusiak.freebloks.R
@@ -162,7 +160,7 @@ fun NewGameScreen(
                     colors = IconButtonDefaults.outlinedIconButtonColors(contentColor = MaterialTheme.colorScheme.primary),
                     enabled = gameMode != GAMEMODE_JUNIOR
                 ) {
-                    Icon(Icons.Filled.Settings, null)
+                    Icon(Icons.Filled.Build, null)
                 }
 
                 Button(
