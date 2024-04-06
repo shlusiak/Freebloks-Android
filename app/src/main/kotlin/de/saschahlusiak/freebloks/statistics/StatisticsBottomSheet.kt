a1701cac859a9462ba69f39c62696b8c5d321ee8
Sascha Hlusiak <mail@saschahlusiak.de>
2024-04-06 22:46:03 +0800

    Fix statistics bottom sheet background in dark mode

--- app/src/main/kotlin/de/saschahlusiak/freebloks/statistics/StatisticsBottomSheet.kt
+++ app/src/main/kotlin/de/saschahlusiak/freebloks/statistics/StatisticsBottomSheet.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import de.saschahlusiak.freebloks.R
 import de.saschahlusiak.freebloks.app.AppTheme
 
 @AndroidEntryPoint
@@ -31,12 +35,17 @@ class StatisticsBottomSheet : BottomSheetDialogFragment() {
     @Composable
     private fun Content() {
         AppTheme {
-            StatisticsContent(
-                modifier = Modifier.padding(top = 16.dp),
-                gameMode = viewModel.gameMode.collectAsState().value,
-                data = viewModel.data.collectAsState().value,
-                onGameMode = { viewModel.gameMode.value = it }
-            )
+            val cornerRadius = dimensionResource(id = R.dimen.dialog_corner_radius)
+            Surface(
+                shape = RoundedCornerShape(topStart = cornerRadius, topEnd = cornerRadius)
+            ) {
+                StatisticsContent(
+                    modifier = Modifier.padding(top = 16.dp),
+                    gameMode = viewModel.gameMode.collectAsState().value,
+                    data = viewModel.data.collectAsState().value,
+                    onGameMode = { viewModel.gameMode.value = it }
+                )
+            }
         }
     }
 }
