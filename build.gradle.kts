ca94887fedb9b795c6a1c01b975e74dab4b49ddf
Sascha Hlusiak <mail@saschahlusiak.de>
2025-03-30 11:16:51 +0800

    Bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,12 @@ buildscript {
 
     dependencies {
         // https://developer.android.com/studio/releases/gradle-plugin
-        classpath ("com.android.tools.build:gradle:8.9.0")
+        classpath ("com.android.tools.build:gradle:8.9.1")
 
         // Kotlin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.1.20")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.1.20-1.0.31")
 
         // Hilt
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.53")
@@ -21,7 +22,6 @@ buildscript {
         // https://developers.google.com/android/guides/releases
         classpath("com.google.gms:google-services:4.4.2")
         classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.3")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.20-1.0.24")
     }
 }
 
