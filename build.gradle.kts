a7d6bdb837ec128b269e90d4e33e608bd810a352
Sascha Hlusiak <mail@saschahlusiak.de>
2026-06-18 15:50:30 +0800

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.services) apply false
