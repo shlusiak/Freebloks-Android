c8b6f2b2b6880f6c87eaaab4be6861d9680766b3
Sascha Hlusiak <mail@saschahlusiak.de>
2026-08-01 12:44:31 +0800

    Release 1.8.2 (vc 182)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         ndkVersion = libs.versions.ndkVersion.get()
 
-        versionCode = 181
-        versionName = "1.8.1"
+        versionCode = 182
+        versionName = "1.8.2"
 
         @Suppress("UnstableApiUsage")
         androidResources.localeFilters += setOf("en", "de", "es", "fr", "ja", "pt", "ro", "ru", "zh-rHK")
